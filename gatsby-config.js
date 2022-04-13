fc2cb313ae559cddd513f163e804792139713167
Matt Barbier <matt.barbier@puregym.com>
2022-04-13 14:56:53 +0100

    Part 7 completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-mdx`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-image`,    
     {
       resolve: "gatsby-source-filesystem",
